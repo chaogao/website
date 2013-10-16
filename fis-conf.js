f9a2b37f1e77ac49c2fa6e9209a7312d583a5a9f
gaochao <824310342@qq.com>
2013-10-16 10:48:50 +0800

    init

---
+++ fis-conf.js
@@ -0,0 +1,41 @@
+fis.config.merge({
+    modules : {
+        parser : {
+            less : 'less'
+        }
+    },
+    roadmap : {
+        ext : {
+            less : 'css'
+        },
+        path : [
+            {
+                reg : /^\/views\/(.*\.ejs)/i,
+                release : '/views/$1',
+                useCompil: true,
+                useMap: true,
+                isHtmlLike: true,
+                isJsLike: true,
+                isCssLike: true,
+            },
+            {
+                reg : /^\/public\/(.*\..*)$/i,
+                release : '/public/$1'
+            },
+            {
+                reg : /^\/(node_modules|routes|app.js)/i,
+                useCompile: false,
+                isHtmlLike: false,
+                isJsLike: false,
+                isCssLike: false,
+                useHash: false,
+                useDomain: false,
+                useMap: false,
+                useSprite: false
+            }
+        ]
+    },
+    pack: {
+        'public/package/all.css': [/^\/public\/.*\.(less|css)/]
+    }
+});
