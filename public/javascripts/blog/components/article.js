93c4012afe2ea7140822992fc69b27ca98f40103
gaochao <824310342@qq.com>
2013-10-17 18:19:46 +0800

    article

--- public/javascripts/blog/components/article.js
+++ public/javascripts/blog/components/article.js
@@ -7,6 +7,8 @@
     Article = function () {
         var self = this;
 
+        self.content = $(".blog-article");
+
         $(window).resize(function () {
             if (self.timer) {
                 clearTimeout(self.timer);
@@ -16,6 +18,9 @@
                 self.resizeBg();
             }, 100);
         });
+
+        self.deleagesEvents();
+        self.getArticle();
     };
 
     $.extend(Article.prototype, {
@@ -27,13 +32,56 @@
                 height = parseInt($(window).height()),
                 rHeight, rWidth, seed;
 
-            rHeight = height - $(".blog-article").offset().top - 28;
+            rHeight = height - $(".blog-article").offset().top - 8;
             rWidth = $(".blog-article-cover").width();
 
             $(".blog-article-cover").height(rHeight);
 
-            $(".blog-article-cover .read").css("left", parseInt(Math.random(0.2, 0.8) * (rWidth - 100)) + "px");
-            $(".blog-article-cover .read").css("top", parseInt(Math.random(0.2, 0.8) * (rHeight - 100)) + "px");
+            $(".blog-article-cover .read").css("left", parseInt(Math.random(0.2, 0.8) * (rWidth - 150)) + "px");
+            $(".blog-article-cover .read").css("top", parseInt(Math.random(0.2, 0.8) * (rHeight - 150)) + "px");
+        },
+        /**
+         * 获取article详情
+         */
+        getArticle: function () {
+            var self = this;
+
+            $.get("/public/test/article.json", function (json) {
+                self.articleData = json;
+                $(self).trigger("recivedata", [{data: json}]);
+            });
+        },
+        /**
+         * 绑定事件
+         */
+        deleagesEvents: function () {
+            var self = this;
+
+            self.content.delegate(".read, .infomation a", "click", function() {
+                if (self.articleData) {
+                    self.initArticle();
+                } else {
+                    $(self).one("recivedata", function () {
+                        self.initArticle();
+                    });
+                }
+            });
+        },
+        /** 
+         * 初始化article
+         */
+        initArticle: function () {
+            var self = this,
+                data = self.articleData,
+                dfd;
+
+            dfd = $.Deferred(function (dfd) {
+                self.content.find(".blog-article-cover").fadeOut(500, function () {
+                    dfd.resolve();
+                });
+            }).done(function () {
+                self.content.find(".blog-article-content").html(data.content);
+            });
         }
     });
 
--- views/blog/components/article.tpl
+++ views/blog/components/article.tpl
@@ -1,8 +1,11 @@
 <div class="blog-article">
     <div class="blog-article-category"></div>
-    <div class="blog-article-content">
-        <div class="blog-article-cover"  style="background-image: url(/public/upload/KingPengiunsSeal_ZH-CN10941640086_1366x768.jpg)">
-            <a class="read" href="javascript:void(0);">Read</a>
+    <div class="blog-article-content"></div>
+    <div class="blog-article-cover"  style="background-image: url(/public/upload/KingPengiunsSeal_ZH-CN10941640086_1366x768.jpg)">
+        <div class="infomation">
+            <h2><a href="javascript:void(0);">less——可编程的css语言</a></h2>
+            <span class="description">LESSCSS是一种动态样式语言，属于CSS预处理语言的一种，它使用类似CSS的语法，为CSS的赋予了动态语言的特性，如变量、继承、运算、函数等，更方便CSS的编写和维护。</span>
         </div>
+        <a class="read" href="javascript:void(0);">Read</a>
     </div>
 </div>
